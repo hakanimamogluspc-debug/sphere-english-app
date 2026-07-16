417b7ede41ebd889a18e23c85d7c5f0da0ef1773
Hakan Imamoglu <hakanimamogluspc@gmail.com>
2026-07-16 17:08:53 +0300

    fix(luca): Receiver.EMail alani direct (Address.EMail'den ayri)

--- artifacts/api-server/src/lib/invoice/luca-provider.ts
+++ artifacts/api-server/src/lib/invoice/luca-provider.ts
@@ -347,6 +347,7 @@ ${paymentNote}
                 <ein:TownName>${xmlEscape(buyer.district ?? "")}</ein:TownName>
                 <ein:EMail>${xmlEscape(buyer.email)}</ein:EMail>
               </ein:Address>
+              <ein:EMail>${xmlEscape(buyer.email)}</ein:EMail>
               <ein:ReceiverName>${xmlEscape(receiverName)}</ein:ReceiverName>
               <ein:ReceiverTaxCode>${xmlEscape(receiverTaxCode)}</ein:ReceiverTaxCode>
               <ein:SendingType>ELEKTRONIK</ein:SendingType>
